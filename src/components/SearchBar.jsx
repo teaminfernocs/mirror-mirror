464a9f0114237322ac1444d2f97914492009d99a
Mia <ngocmhuynh5@gmail.com>
2018-07-11 15:08:05 -0700

    SearchBar still needs functionality to add to Search Results component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,36 +6,36 @@ import Button from '@material-ui/core/Button';
 
 export default class SearchBar extends Component {
 	constructor(props) {
-			super(props);
-			
-			this.state = {
-				searchField: '',
-			}
-			this.onSearchChange = this.onSearchChange.bind(this);
+		super(props);
+
+		this.state = {
+		searchField: '',
 	}
-	
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
 	onSearchChange(event) {
 		this.setState({searchField: event.target.value});
 	}
 
 	render() {
-		return (
-			<MuiThemeProvider theme={theme}>
-				<div className='search-bar'>
-					<TextField
-						label="Search"
-						value={this.state.searchField}
-						onChange={this.onSearchChange}
-						required
-					/>
-					<Button
-					color="primary"
-					variant="contained"
-					>
-					Submit
-					</Button>
-				</div>
-			</MuiThemeProvider>
+	return (
+	<MuiThemeProvider theme={theme}>
+		<div className='search-bar'>
+			<TextField
+				label="Search"
+				value={this.state.searchField}
+				onChange={this.onSearchChange}
+				required
+			/>
+			<Button
+			color="primary"
+			variant="contained"
+			>
+			Submit
+			</Button>
+		</div>
+	</MuiThemeProvider>
 		)
 	}
 }
