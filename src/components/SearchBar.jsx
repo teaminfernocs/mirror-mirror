9a5baef5be69daf315ecc4fd39fbf2062b977af7
Mia <ngocmhuynh5@gmail.com>
2018-07-11 10:57:09 -0700

    created new searchbar component as a stateful class component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { Component} from 'react';
 import TextField from '@material-ui/core/TextField';
-import Button from '@matierl-ui/core/Button';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from '../theme';
-import Button from '@material-ui/core/Button';
 
 
 const SearchBar = ({ searchField, searchChange }) => {
-    return (
-        <div className='search-bar'>
-        <input 
-            className='text-box'
-            type='search'
-            placeholder='Search questions'
-            value={searchField}
-            onChange={searchChange}
-            />
-        </div>
-    )
+	return (
+			<div className='search-bar'>
+			<input 
+					className='text-box'
+					type='search'
+					placeholder='Search questions'
+					value={searchField}
+					onChange={searchChange}
+					/>
+			</div>
+	)
 };
 
 export default SearchBar;
+
+
+class newSearchBar extends Component {
+	constructor(props) {
+			super(props);
+			
+			this.state = {
+				searchField: '',
+			}
+			this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	
+	onSearchChange(event) {
+		this.setState({searchField: event.target.value});
+	}
+
+	render() {
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div className='search-bar'>
+					<TextField
+						label='Search Questions'
+						value={searchField}
+						onChange={this.onSearchChange}
+						required
+					/>
+				</div>
+			</MuiThemeProvider>
+		)
+	}
+}
