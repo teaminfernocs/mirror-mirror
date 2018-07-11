0b6afce49e77f8a1e66bd5dc34df1b1ea9912710
Mia <ngocmhuynh5@gmail.com>
2018-07-10 19:09:17 -0700

    downloaded npm files and added skeleton for searchbar ui component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className='search-bar'>
+        <input 
+            className='text-box'
+            type='search'
+            placeholder='Search questions'
+            />
+        </div>
+    )
+};
+
+export default SearchBar;
